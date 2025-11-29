3f222bbf1ba6ddc10deb7a9b2e53423557acbe07
Joseph <josephsik072507@gmail.com>
2025-11-28 22:01:03 -0500

    Update main.py

--- Where is Doggo/main.py
+++ Where is Doggo/main.py
@@ -1,1 +1,12 @@
+import pygame
+from src.controller import GameController
 
+def main():
+    pygame.init()
+    pygame.mixer.init()
+
+    controller = GameController()
+    controller.run()
+
+if __name__ == "__main__":
+    main()
